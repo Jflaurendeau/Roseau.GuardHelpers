33eb1fcb7f5510fd599eb9d29178ed628bb18d6f
Jean-François Laurendeau <laurendeau.jf@outlook.com>
2022-04-09 23:14:23 -0400

    Simple test added to test the git actions et ci.yaml pipeline.

---
+++ tests/Roseau.GuardHelpers.UnitTests/GuardExtensionTests.cs
@@ -0,0 +1,69 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Roseau.GuardHelpers.UnitTests
+{
+    [TestClass]
+    public class GuardExtensionTests
+    {
+        [TestMethod]
+        public void IsLessThanOrEqualTo_BothDatesAreEquals_NoThrow()
+        {
+            // Arrange
+            DateOnly firstDate = new DateOnly(2020, 1, 1);
+
+            // Act and Assert
+            GuardExtension.IsLessThanOrEqualTo(firstDate, nameof(firstDate), firstDate, nameof(firstDate));
+        }
+        [TestMethod]
+        public void IsLessThanOrEqualTo_FirstDateBeforeSecondDate_NoThrow()
+        {
+            // Arrange
+            DateOnly firstDate = new DateOnly(2020, 1, 1);
+            DateOnly secondDate = firstDate.AddDays(1);
+
+            // Act and Assert
+            GuardExtension.IsLessThanOrEqualTo(firstDate, nameof(firstDate), secondDate, nameof(secondDate));
+        }
+        [TestMethod]
+        public void IsLessThanOrEqualTo_FirstDateLaterThanSecondDate_Throws()
+        {
+            // Arrange
+            DateOnly firstDate = new DateOnly(2020, 1, 1);
+            DateOnly secondDate = firstDate.AddDays(-1);
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => GuardExtension.IsLessThanOrEqualTo(firstDate, nameof(firstDate), secondDate, nameof(secondDate)));
+        }
+        [TestMethod]
+        public void DatesAreInAscendingOrder_EveryDaysAreInChronologicSequence_NoThrow()
+        {
+            // Arrange
+            DateOnly[] dates = new DateOnly[]
+            {
+                new DateOnly(2020, 1, 1),
+                new DateOnly(2020, 1, 2),
+                new DateOnly(2020, 1, 3),
+                new DateOnly(2020, 1, 4)
+            };
+
+            // Act and Assert
+            GuardExtension.DatesAreInAscendingOrder(dates, nameof(dates));
+        }
+        [TestMethod]
+        public void DatesAreInAscendingOrder_AtLeastOneDateIsNotInChronological_Throws()
+        {
+            // Arrange
+            DateOnly[] dates = new DateOnly[]
+            {
+                new DateOnly(2020, 1, 1),
+                new DateOnly(2020, 1, 2),
+                new DateOnly(2020, 1, 4),
+                new DateOnly(2020, 1, 3)
+            };
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => GuardExtension.DatesAreInAscendingOrder(dates, nameof(dates)));
+        }
+    }
+}
