a1acbd27d599a1f7ec5af289adc6c66df8e684ed
Jean-François Laurendeau <laurendeau.jf@outlook.com>
2022-04-09 22:14:10 -0400

    Ajout d'un projet et d'action GitHub.

---
+++ scr/Roseau.GuardHelpers/GuardExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace Roseau.GuardHelpers;
+
+public static class GuardExtension
+{
+    public static void IsLessThanOrEqualTo(DateOnly firstDate, string firstDateName, DateOnly secondDate, string secondDateName)
+    {
+        if (firstDate > secondDate)
+            throw new ArgumentOutOfRangeException(firstDateName, $"The {firstDateName} ({firstDate}) must be a prior or equal date than the {secondDateName} ({secondDate}).");
+    }
+    public static void DatesAreInAscendingOrder(DateOnly[] dates, string datesName)
+    {
+        if (dates.Length < 2)
+            return;
+        for (int i = 1; i < dates.Length; i++)
+        {
+            IsLessThanOrEqualTo(dates[i - 1], $"Index {i - 1} of {datesName}", dates[i], $"Index {i} of {datesName}");
+        }
+    }
+}
