c1a9a13500f29a4d0c9e6003c9fb71b97f8c3b6f
Jean-François Laurendeau <laurendeau.jf@outlook.com>
2022-04-23 19:25:03 -0400

    Learning IEnumerable mecanics.

--- scr/Roseau.GuardHelpers/GuardExtension.cs
+++ scr/Roseau.GuardHelpers/GuardExtension.cs
@@ -1,4 +1,8 @@
-﻿namespace Roseau.GuardHelpers;
+﻿using Microsoft.Toolkit.Diagnostics;
+using System.Collections;
+using System.Linq;
+
+namespace Roseau.GuardHelpers;
 
 public static class GuardExtension
 {
@@ -14,6 +18,68 @@ public static class GuardExtension
         for (int i = 1; i < dates.Length; i++)
         {
             IsLessThanOrEqualTo(dates[i - 1], $"Index {i - 1} of {datesName}", dates[i], $"Index {i} of {datesName}");
+        }
+    }
+    public static void HaveSameLength(IEnumerable firstEnumerable, string firstEnumerableName, IEnumerable secondEnumerable, string secondEnumerableName)
+    {
+        if (firstEnumerable.Count(firstEnumerableName) != secondEnumerable.Count(secondEnumerableName))
+            throw new ArgumentOutOfRangeException(firstEnumerableName, $"The {firstEnumerableName} and {secondEnumerableName} must have the same length or count.");
+    }
+    private static int Count(this IEnumerable enumerable, string enumerableName)
+    {
+        Guard.IsNotNull(enumerable, nameof(enumerable));
+        if (enumerable is ICollection collection)
+            return collection.Count;
+        
+        int count = 0;
+        var iEnumerator = enumerable.GetEnumerator();
+        CustomUsingForAgurmentException(iEnumerator, () =>
+        {
+            checked
+            {
+                while (iEnumerator.MoveNext())
+                    count++;
+            }
+        }, enumerableName);
+        return count;
+    }
+    private static void CustomUsingForAgurmentException(object obj, Action action, string variableName)
+    {
+        try
+        {
+            action();
+        }
+        catch (OverflowException)
+        {
+            throw new ArgumentOutOfRangeException(variableName, $"The {variableName} has a number of item that exceeds the range of {typeof(int)} type.");
+        }
+        finally
+        {
+            if (obj is IDisposable disposable)
+                disposable.Dispose();
+        }
+    }
+
+    public class CustomUsingForAgurment : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Blabla : IEnumerator
+    {
+        public object Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
         }
     }
 }
